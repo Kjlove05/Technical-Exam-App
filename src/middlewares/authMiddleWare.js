0199aebed6bd5641150d8d8f209d53dbc0238f7d
Kyle Joshua Ronquillo <kylejoshuaronquillo@Kyles-MacBook-Air.local>
2024-01-23 02:35:39 +0800

    Added Middleware Authentication

---
+++ src/middlewares/authMiddleWare.js
@@ -0,0 +1,28 @@
+const expressAsyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const User = require('../model/User');
+
+
+const authMiddleware = expressAsyncHandler( async(req,res,next) => {
+  let token;
+
+  if(req?.headers?.authorization?.startsWith("Bearer")) {
+    token = req?.headers?.authorization?.split(" ")[1];
+    try{
+      if(token) {
+        const decodedUser = jwt.verify(token, process.env.JWT_KEY);
+        const user = await User.findById(decodedUser?.id);
+        req.user = user;
+        next();
+      }
+     } catch(err){
+      throw new Error("Not Authorized token expired");
+
+    }
+  } else {
+    throw new Error("There is no token attched to the header");
+
+  }
+});
+
+module.exports = authMiddleware;
